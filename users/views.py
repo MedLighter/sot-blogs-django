d446ab033aa66e9681223a868e53c94b14f80b99
MedLighter <ilyaudalov2004@yandex.ru>
2023-03-12 17:27:19 +0300

    add blogs

--- users/views.py
+++ users/views.py
@@ -1,13 +1,54 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context={
+        'form': form,
+        'title': 'Login'
+    }
+    return render(request, 'users/login.html', context)
 
 
 def registration(request):
-    return render(request, 'users/registration.html')
+    if request.method == "POST":
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+    context = {
+        'form': form,
+        'title': 'Registration'
+    }
+    return render(request, 'users/registration.html', context)
 
 
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {
+        'form': form,
+        'title': 'Profile'
+    }
+    return render(request, 'users/profile.html', context)
